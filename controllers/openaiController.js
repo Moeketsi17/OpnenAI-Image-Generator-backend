ad8d454d77452e8b62a3a6629730dc9c4b191bd5
moeketsi <raymondkxng17@gmail.com>
2023-01-12 19:26:36 +0200

    error handling

--- controllers/openaiController.js
+++ controllers/openaiController.js
@@ -7,9 +7,32 @@ const Configuration = new Configuration({
 const openai = new OpenAIApi(Configuration);
 
 const generateImage = async (req, res) => {
-    res.status(200).json({
-        success: true,
-    });
+    try {
+        const response = await openai.createImage({
+            prompt: "Nelson Mandela wearing gold cuban links",
+            n: 1,
+            size: "512x512"
+        });
+
+        const imageUrl = response.data.data[0].url
+
+        res.status(200).json({
+            success: true,
+            data: imageUrl,
+        })
+    } catch (error){
+        if (error.response){
+            console.log(error.response.status);
+            console.log(error.response.data);
+        } else {
+            console.log(error.message)
+        }
+
+        res.status(400).json({
+            success: false,
+            error: "The image could not be generated",
+        });
+    }
 }
 
 
