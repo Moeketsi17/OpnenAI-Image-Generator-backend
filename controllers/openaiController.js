9eedb94a4bea5db99a98575600b6ff6f43cbc041
moeketsi <raymondkxng17@gmail.com>
2023-01-12 19:14:11 +0200

    commit

---
+++ controllers/openaiController.js
@@ -0,0 +1,16 @@
+const {Configuration, OpenAIApi} = require("openai");
+
+const Configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(Configuration);
+
+const generateImage = async (req, res) => {
+    res.status(200).json({
+        success: true,
+    });
+}
+
+
+module.exports = {generateImage};
