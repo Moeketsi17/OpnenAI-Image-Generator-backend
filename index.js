521ddd97c4569b64fe365d3075ee51dc49218b1d
moeketsi <raymondkxng17@gmail.com>
2023-01-13 18:01:27 +0200

    cors installed

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const dotenv = require("dotenv").config();
 const port = process.env.PORT || 5000;
 const app = express();
 const path =  require("path")
+const cors = require("cors");
+
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
