4c4d2d2b7cbf6e504d6e97df5608bf89ec38d202
moeketsi <raymondkxng17@gmail.com>
2023-01-13 10:13:04 +0200

    generate images

--- controllers/openaiController.js
+++ controllers/openaiController.js
@@ -1,43 +1,41 @@
-const {Configuration, OpenAIApi} = require("openai");
+const { Configuration, OpenAIApi } = require('openai');
 
 const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
 });
-
-const openai = new OpenAIApi(Configuration);
+const openai = new OpenAIApi(configuration);
 
 const generateImage = async (req, res) => {
-    const {promt, size} = req.body;
+    const { prompt, size } = req.body;
 
-    const imageSize = size === "small" ? "256x256" : size === "medium" ? "512x512" : "1024x1024"
+    const imageSize = size === 'small' ? '256x256' : size === 'medium' ? '512x512' : '1024x1024';
 
     try {
         const response = await openai.createImage({
             prompt,
             n: 1,
-            size: "512x512"
+            size: imageSize,
         });
 
-        const imageUrl = response.data.data[0].url
+        const imageUrl = response.data.data[0].url;
 
         res.status(200).json({
             success: true,
             data: imageUrl,
-        })
-    } catch (error){
-        if (error.response){
+        });
+    } catch (error) {
+        if (error.response) {
             console.log(error.response.status);
             console.log(error.response.data);
         } else {
-            console.log(error.message)
+            console.log(error.message);
         }
 
         res.status(400).json({
             success: false,
-            error: "The image could not be generated",
+            error: 'The image could not be generated',
         });
     }
-}
+};
 
-
-module.exports = {generateImage};
+module.exports = { generateImage };
--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 5000;
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
 app.use("/openai", require("./routes/openaiRoutes"));
 app.listen(port, () => console.log(`Server running on port ${port}`));
